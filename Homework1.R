36f37816a3c286024237ad4af02b891743f7605c
Paul ReFalo <paul.refalo@gmail.com>
2017-09-24 15:45:28 -0700

    ST516 - Foundations of Data Analytics
    Foundations of Data Analytics with Charlotte Wickham.  Fall 2017 first
    quarter of Data Analytics MS at OSU.

---
+++ Homework1.R
@@ -0,0 +1,66 @@
+# ST 516 - Homework 1 ========
+
+# This file will help guide you through the first problem on Homework 1.
+# Text after a hash (#) without a quote is interpreted as a code comment.
+# Anything else is interpreted as R code.
+
+
+# Problem 1 ========
+
+# Part (a)  ========
+
+# Example
+# Addition: three plus four
+3 + 4
+
+# Your turn...
+# Exponentiation: Three to the second power 
+
+
+# Part (b)  ========
+
+# Example 
+example1 <- 10
+example1 
+example2 <- "twenty"
+example2
+class(example1)
+class(example2)
+
+# Your turn...
+one <- 31.28
+# Don't forget two and three!
+  
+
+
+class(one)
+# Now you do the next two
+
+
+# Part (c)  ========
+
+# Example
+example <- c(1, 3, 2, 4)
+names(example) <- c("odd1", "odd2", "even1", "even2")
+example
+
+# Your turn...
+
+
+
+
+# Part (d)  ========
+
+# Example 
+prod(example) # prod() gives the product of the elements of a vector
+example[c(2,4)] # This gives the 2nd and 4th elements of the "example" vector
+example < 3
+
+# Your turn...
+
+
+
+
+
+# Now restart your R session. Highlight all of your code, and click Run. 
+# Did it run without any errors?
